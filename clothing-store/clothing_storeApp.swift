5504e6757289363cbf93c08b38530dcaff338e86
ksowah <sowahkelvin640@gmail.com>
2025-04-10 10:54:52 +0000

    Initial Commit

---
+++ clothing-store/clothing_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothing_storeApp.swift
+//  clothing-store
+//
+//  Created by macbook on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct clothing_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
